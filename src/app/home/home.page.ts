5bfa8c43dcc948b9cae8a0ca058074d03b8491a7
Amagaraï Guindo <amagarai6@gmail.com>
2022-12-29 14:10:22 +0000

    tictacto finish sans design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,99 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  animations: [
+    trigger('enterAnimation', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate('500ms', style({opacity:1}))
+      ]),
+      transition(':leave', [
+        style({opacity:1}),
+        animate('500ms', style({opacity:0}))
+      ])
+    ])
+  ]
 })
 export class HomePage {
 
+  // Tableau représentant l'état de chaque case de la grille
+  grid: string[][] = [['', '', ''], ['', '', ''], ['', '', '']];
+  // Indique si c'est à X ou à O de jouer
+  currentPlayer: 'X' | 'O' = 'X';
+  // Indique si la partie est terminée
+  gameOver: boolean = false;
+  // Stocke le gagnant (X ou O) si la partie est terminée
+  winner: 'X' | 'O' | any= null
+
   constructor() {}
+
+
+
+  play(row: number, col: number) {
+    // Si la partie est terminée ou si la case est déjà occupée, on ne fait rien
+    if (this.gameOver || this.grid[row][col] !== '') {
+      return;
+    }
+    // On met à jour l'état de la grille et de la partie
+    this.grid[row][col] = this.currentPlayer;
+    this.checkGameOver();
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
+
+
+  checkGameOver() {
+    // Vérifie si une ligne est complète et contient les mêmes symboles
+    for (let i = 0; i < 3; i++) {
+      if (this.grid[i][0] !== '' && this.grid[i][0] === this.grid[i][1] && this.grid[i][1] === this.grid[i][2]) {
+        this.gameOver = true;
+        this.winner = this.grid[i][0];
+        return;
+      }
+    }
+    // Vérifie si une colonne est complète et contient les mêmes symboles
+    for (let i = 0; i < 3; i++) {
+      if (this.grid[0][i] !== '' && this.grid[0][i] === this.grid[1][i] && this.grid[1][i] === this.grid[2][i]) {
+        this.gameOver = true;
+        this.winner = this.grid[0][i];
+        return;
+      }
+    }
+    // Vérifie si la première diagonale est complète et contient les mêmes symboles
+    if (this.grid[0][0] !== '' && this.grid[0][0] === this.grid[1][1] && this.grid[1][1] === this.grid[2][2]) {
+      this.gameOver = true;
+      this.winner = this.grid[0][0];
+      return;
+    }
+    // Vérifie si la seconde diagonale est complète et contient les mêmes symboles
+    if (this.grid[0][2] !== '' && this.grid[0][2] === this.grid[1][1] && this.grid[1][1] === this.grid[2][0]) {
+      this.gameOver = true;
+      this.winner = this.grid[0][2];
+      return;
+    }
+    // Vérifie s'il y a un match nul
+    let full = true;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (this.grid[i][j] === '') {
+          full = false;
+          break;
+        }
+      }
+    }
+    if (full) {
+      this.gameOver = true;
+    }
+  }
+
+  reset() {
+    this.grid = [['', '', ''], ['', '', ''], ['', '', '']];
+    this.currentPlayer = 'X';
+    this.gameOver = false;
+    this.winner = null;
+  }
 
 }
